78f2b01e82ed12763eb6db846cec123836cbe6d2
Samuel <46892227+yong-an@users.noreply.github.com>
2019-09-09 23:27:06 +0800

    Updated Api Keys

--- chas_explorer/app/src/main/java/com/example/chas_explorer/MapsActivity.java
+++ chas_explorer/app/src/main/java/com/example/chas_explorer/MapsActivity.java
@@ -40,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng SG = new LatLng(1.3521, 103.8198);
+        mMap.addMarker(new MarkerOptions().position(SG).title("Marker in Singapore"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SG));
     }
 }
