1a039e26b304b98e16a15ca651527d9d104cb3a0
Samuel <46892227+yong-an@users.noreply.github.com>
2019-09-14 15:57:45 +0800

    Map + Firebase Updated

--- Android Studio Project/app/src/main/java/com/example/chasexplorer/MapsActivity.java
+++ Android Studio Project/app/src/main/java/com/example/chasexplorer/MapsActivity.java
@@ -35,13 +35,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Singapore = new LatLng(-1.290270, 103.851959);
+        mMap.addMarker(new MarkerOptions().position(Singapore).title("Marker in Singapore"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Singapore));
     }
 }
