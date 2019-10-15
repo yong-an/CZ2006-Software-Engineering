98f5087dd996fee40bb8ceb4616a8f9ddcc10a55
toygg96 <josephkingyiu19@gmail.com>
2019-10-15 14:13:22 +0800

    Sample URL update

--- CZ2006 Cleaning Data Application/src/Control/TextDB.java
+++ CZ2006 Cleaning Data Application/src/Control/TextDB.java
@@ -253,6 +253,7 @@ public class TextDB {
 	private static Clinic getPlaceID(Clinic c) {
 		c.setClinicName(c.getClinicName().replaceAll("&amp;", "&"));
 		String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36";
+		//https://maps.googleapis.com/maps/api/place/details/json?place_id=<INSERT PLACE ID>&fields=name,opening_hours&key=AIzaSyAIBxxcXjzv456wmYzLDuWJ03zXte9agMc
 		String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + c.getXCoordinate() + "," + c.getYCoordinate() + "&radius=50&type=clinic&keyword=" + c.getClinicName() +  "&key=" + API_KEY;
 		try {
 			url = url.replaceAll(" ", "%20");
